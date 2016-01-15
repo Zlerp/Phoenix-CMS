b1df6e8403fb46de5fb0a289ec22f76ed35b8290
Zlerp <zhpdeals@gmail.com>
2016-01-15 17:53:21 -0500

    DRYing up

--- app/helpers/youtube_helper.rb
+++ app/helpers/youtube_helper.rb
@@ -2,8 +2,5 @@ module YoutubeHelper
   def embedVideo(youtube_url)
     youtube_id = youtube_url.split("=").last
     content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}", id: "modal-iframe")
-    # content_tag(:p, "//www.youtube.com/embed/#{youtube_id}")
   end
-
-
 end
